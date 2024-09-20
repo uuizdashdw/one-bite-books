9fb768a25aaa571597c2cd86a8f053833651f70e
uuizdashdw <chany920@gmail.com>
2024-09-20 19:15:16 +0900

    1. search 페이지 구현 완료

--- section03/src/app/(with-searchbar)/search/page.tsx
+++ section03/src/app/(with-searchbar)/search/page.tsx
@@ -1,16 +1,24 @@
-// Mock Data
-import books from '@/mock/books.json';
-
 // Component
 import BookItem from '@/components/book-item';
 
-export default function Page({
+// Type
+import { BookData } from '@/types';
+
+export default async function Page({
 	searchParams,
 }: {
 	searchParams: {
 		q?: string;
 	};
 }) {
+	const response = await fetch(
+		`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${searchParams.q}`,
+	);
+
+	if (!response.ok) return <div>오류가 발생했습니다...</div>;
+
+	const books: BookData[] = await response.json();
+
 	return (
 		<div>
 			{books.map(book => (
