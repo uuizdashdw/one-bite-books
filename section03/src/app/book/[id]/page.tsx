e733fa043f184140c487e8372288f5af811484b6
uuizdashdw <chany920@gmail.com>
2024-10-10 18:59:15 +0900

    1. 서버 액션을 통해 도서 리뷰 기능 추가
    2. CSS 수정

--- section03/src/app/book/[id]/page.tsx
+++ section03/src/app/book/[id]/page.tsx
@@ -8,13 +8,10 @@ export function generateStaticParams() {
 	return [{ id: '1' }, { id: '2' }, { id: '3' }];
 }
 
-export default async function Page({
-	params,
-}: {
-	params: { id: string | string[] };
-}) {
+// 도서 상세 정보
+async function BookDetail({ bookId }: { bookId: string }) {
 	const response = await fetch(
-		`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${params.id}`,
+		`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${bookId}`,
 	);
 
 	if (!response.ok) {
@@ -28,7 +25,7 @@ export default async function Page({
 		book;
 
 	return (
-		<div className={style.container}>
+		<section>
 			<div
 				className={style.cover_img_container}
 				style={{ backgroundImage: `url('${coverImgUrl}')` }}
@@ -41,6 +38,54 @@ export default async function Page({
 				{author} | {publisher}
 			</div>
 			<div className={style.description}>{description}</div>
+		</section>
+	);
+}
+
+// 도시 리뷰 에디터
+function ReviewEditor({ bookId }: { bookId: string }) {
+	async function createReviewAction(formData: FormData) {
+		'use server';
+
+		const content = formData.get('content')?.toString();
+		const author = formData.get('author')?.toString();
+
+		if (!content || !author) return;
+
+		// 도서 리뷰 추가 기능
+		try {
+			const response = await fetch(
+				`${process.env.NEXT_PUBLIC_API_SERVER_URL}/review`,
+				{
+					method: 'POST',
+					body: JSON.stringify({ content, author, bookId }),
+				},
+			);
+
+			console.log(response.status);
+		} catch (err) {
+			console.error(err);
+			return;
+		}
+	}
+
+	return (
+		<section>
+			<form action={createReviewAction}>
+				<input type="text" name="content" placeholder="리뷰 내용" required />
+				<input type="text" name="author" placeholder="작성자" required />
+				<button type="submit">작성하기</button>
+			</form>
+		</section>
+	);
+}
+
+// 도서 상세 페이지
+export default async function Page({ params }: { params: { id: string } }) {
+	return (
+		<div className={style.container}>
+			<BookDetail bookId={params.id} />
+			<ReviewEditor bookId={params.id} />
 		</div>
 	);
 }
