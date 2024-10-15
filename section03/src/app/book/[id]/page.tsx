83d20a10b916d8a64d40e211f792a8a9f788c6f3
uuizdashdw <chany920@gmail.com>
2024-10-15 19:01:57 +0900

    1. 이미지 최적화

--- section03/src/app/book/[id]/page.tsx
+++ section03/src/app/book/[id]/page.tsx
@@ -11,6 +11,8 @@ import { ReviewEditor } from '@/components/review-editor';
 import { ReviewData } from '@/types';
 import ReviewItem from '@/components/review-item';
 
+import Image from 'next/image';
+
 export function generateStaticParams() {
 	return [{ id: '1' }, { id: '2' }, { id: '3' }];
 }
@@ -37,7 +39,12 @@ async function BookDetail({ bookId }: { bookId: string }) {
 				className={style.cover_img_container}
 				style={{ backgroundImage: `url('${coverImgUrl}')` }}
 			>
-				<img src={coverImgUrl} />
+				<Image
+					src={coverImgUrl}
+					width={240}
+					height={300}
+					alt={`도서 ${title}의 표지 이미지`}
+				/>
 			</div>
 			<div className={style.title}>{title}</div>
 			<div className={style.subTitle}>{subTitle}</div>
